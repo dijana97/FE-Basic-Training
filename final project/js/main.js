0db9e7c1eab5baab9e94377b1519c753bcdb5dc6
Dijana Radic <dijana.radic157@gmail.com>
2025-08-15 14:31:45 +0200

    Final

---
+++ final project/js/main.js
@@ -0,0 +1,44 @@
+function openCloseSideMenu() {
+  const buttons = document.querySelectorAll('.left-menu__btn');
+
+  buttons.forEach(button => {
+    button.addEventListener('click', () => {
+      const listItem = button.closest('li');
+      const submenu = listItem.querySelector('ul');
+      const submenuTitle = listItem.querySelector('.left-menu__item');
+
+      const isOpened = submenu.classList.contains('left-menu__item--opened');
+
+      if (isOpened) {
+        submenu.style.display = 'none';
+        submenu.classList.remove('left-menu__item--opened');
+        submenuTitle.classList.remove('item--opened');
+        button.classList.remove('item--opened');
+        button.textContent = '+';
+        button.setAttribute('aria-expanded', 'false');
+      } else {
+        submenu.style.display = 'block';
+        submenu.classList.add('left-menu__item--opened');
+        submenuTitle.classList.add('item--opened');
+        button.classList.add('item--opened');
+        button.textContent = '-';
+        button.setAttribute('aria-expanded', 'true');
+      }
+    });
+  });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  openCloseSideMenu();
+
+  const burger = document.querySelector('.nav__burger');
+  const navList = document.querySelector('.nav__list');
+  const search = document.querySelector('.search');
+
+  if (burger && navList && search) {
+    burger.addEventListener('click', () => {
+    navList.classList.toggle('active');
+    search.classList.toggle('active');
+    burger.classList.toggle('active');
+  })
+}});
